c903e5f5e9555740d3fe5d27dbc6898eb4cd6e9d
Diegobr21 <jdiegobrana@gmail.com>
2020-12-18 17:50:04 -0600

    ImportEmpresas working

--- Models/UsersContext.cs
+++ Models/UsersContext.cs
@@ -13,7 +13,7 @@ namespace CaintraData.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=LAPTOP-CN0JGTHQ\\SQLEXPRESS;Initial Catalog=CaintraDBTest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-CN0JGTHQ\\SQLEXPRESS;Initial Catalog=CaintraDBTest2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
     }
 }
---
+++ Pages/ImportEmpresas.razor.cs
@@ -0,0 +1,109 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BlazorInputFile;
+
+using CsvHelper;
+using Microsoft.AspNetCore.Components;
+using CaintraData.Data;
+using CaintraData.Models;
+using Microsoft.VisualBasic;
+
+namespace CaintraData.Pages
+{
+    public partial class ImportEmpresas
+    {
+        [Inject]
+        private IUsersService UsersService { get; set; }
+
+        [Inject]
+        private IEmpresaService EmpresaService { get; set; }
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+        private IFileListEntry FileEntry { get; set; }
+
+        public void OnFileUploaded(IFileListEntry[] files)
+        {
+            FileEntry = files.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Reads a CSV file which contains information of electronic devices,
+        /// parses the data and uploads the products as records to the database.
+        /// </summary>
+        private async Task UploadFileContentsAsync()
+        {
+
+            var usuarios = await UsersService.GetAllUsuarios();
+            var empresas = await EmpresaService.GetAllEmpresas();
+
+            using var reader = new StreamReader(FileEntry.Data);
+            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            await csvReader.ReadAsync();
+            csvReader.ReadHeader();
+            while (await csvReader.ReadAsync())
+            {
+
+                var empresaName = csvReader.GetField("Razon Social").ToLower();
+                // make upper case the first letter of the string
+                empresaName = char.ToUpper(empresaName[0]) + empresaName.Substring(1);
+
+                var nombrecom = csvReader.GetField("Nombre Comercial");
+                var direccion = csvReader.GetField("Direccion");
+                var munestado = csvReader.GetField("Municipio/Estado");
+                var sitioweb = csvReader.GetField("Sitio Web");
+                var numsocio = csvReader.GetField("Num Socio");
+                var size = csvReader.GetField("Size");
+                var vigencia = csvReader.GetField("Vigente");
+                var membresia = false;
+                if (vigencia.Equals("TRUE"))
+                {
+                     membresia = true;
+                }
+                
+                var empresa = empresas.FirstOrDefault(c => c.RazonSocial == empresaName);
+                // if the company is not in the database
+                if (empresa is null)
+                {
+                    empresa = new Empresa
+                    {
+                        NombreComercial = nombrecom,
+                        RazonSocial = empresaName,
+                        Direccion = direccion,
+                        Municipio_Estado = munestado,
+                        SitioWeb = sitioweb,
+                        NumSocio = numsocio,
+                        Empresa_Size = size,
+                        MembresiaVigente = membresia,
+                        LastUpdate = DateAndTime.Now
+                    };
+                    // add it
+                    await EmpresaService.InsertEmpresa(empresa);
+
+                }
+                else
+                {
+                    empresa.NombreComercial = nombrecom;
+                    empresa.Direccion = direccion;
+                    empresa.Municipio_Estado = munestado;
+                    empresa.SitioWeb = sitioweb;
+                    empresa.NumSocio = numsocio;
+                    empresa.Empresa_Size = size;
+                    empresa.MembresiaVigente = membresia;
+                    empresa.LastUpdate = DateAndTime.Now;
+                   
+                    await EmpresaService.UpdateEmpresa(empresa);
+                }
+
+            }
+
+            NavigationManager.NavigateTo("/");
+        }
+
+
+    }
+}
