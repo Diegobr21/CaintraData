b7a5ddd773779b98a1862f16087274fe8de36e40
Diegobr21 <jdiegobrana@gmail.com>
2021-01-05 11:25:59 -0600

    New Migration including field "Origen" in both models

---
+++ Migrations/20210105172440_Second.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CaintraData.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Origen",
+                table: "UsuariosTable",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Origen",
+                table: "EmpresasTable",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Origen",
+                table: "UsuariosTable");
+
+            migrationBuilder.DropColumn(
+                name: "Origen",
+                table: "EmpresasTable");
+        }
+    }
+}
