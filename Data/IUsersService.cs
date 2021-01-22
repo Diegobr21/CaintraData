ed8a11edbca16a18077e5f3aa83d12d3490e3bf7
Diegobr21 <jdiegobrana@gmail.com>
2021-01-22 11:40:52 -0600

    Call for AllActiveUsuarios in UsuariosTable.razor

--- Data/IUsersService.cs
+++ Data/IUsersService.cs
@@ -9,6 +9,8 @@ namespace CaintraData.Data
     public interface IUsersService
     {
         Task<IEnumerable<Usuario>> GetAllUsuarios();
+        Task<IEnumerable<Usuario>> GetAllActiveUsuarios();
+        Task<IEnumerable<Usuario>> GetAllNonActiveUsuarios();
         Task<Usuario> GetUsuarioDetails(int id);
         Task<bool> InsertUsuario(Usuario usuario);
         Task<bool> UpdateUsuario(Usuario usuario);
